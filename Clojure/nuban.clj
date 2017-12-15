0c56ca9ea9a719a6fdf9fb321f48d3b79b8dcec6
Efe Ariaroo <efeariaroo@gmail.com>
2017-12-16 00:01:39 +0100

    clojure validate-nuban function now works

--- Clojure/nuban.clj
+++ Clojure/nuban.clj
@@ -13,24 +13,23 @@
 (defn validate-nuban [bank-code account-number]
   (if (or (not (string? bank-code)) (not (string? account-number)))
     (throw (Exception. "Bank Code and Account Number must be strings"))
-    (if (or (not (= (count bank-code) 3) 
-             (not (= (count account-number) 10))))
+    (if (or (not (= (count bank-code) 3)) 
+             (not (= (count account-number) 10)))
       (throw (Exception. "Bank code should be 3 characters long
                and account number should be 10 characters long"))
-      (let [check-digit (.charAt account-number 9)
+      (let [check-digit (Character/digit (.charAt account-number 9) 10)
             dictionary [3 7 3 3 7 3 3 7 3 3 7 3]
             account-serial-num (.substring account-number 0 9)
             nuban-account-format (str bank-code account-serial-num)
             nuban-chars (vec (seq nuban-account-format))]
         (loop [check-sum 0
-               curr-index 0
-               curr-nuban-char (nuban-chars 0)]
-          (if (= curr-index (count nuban-chars))
+               curr-index 0]
+          (if (< curr-index (count nuban-chars))
             (recur 
-              (+ check-sum (* curr-nuban-char (nuban-chars curr-index)))
-              (inc curr-index)
-              (nuban-chars (inc curr-index)))
+              (+ check-sum (* (Character/digit (nuban-chars curr-index) 10) 
+                              (dictionary curr-index)))
+              (inc curr-index))
             (let [validated-check-digit (- 10 (mod check-sum 10))
-                  final-validated-check-digit (if (= 0 validated-check-digit) 
+                  final-validated-check-digit (if (== 10 validated-check-digit) 
                                                 0 validated-check-digit)]
-              (= check-digit final-validated-check-digit))))))))
+              (== check-digit final-validated-check-digit))))))))
