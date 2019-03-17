681ae2ccd6144b15fc87f6417b61c69e4b87fbd7
timolinn <timothyonyiuke@gmail.com>
2019-03-17 01:44:03 +0100

    Added validation and comments

--- Rust/NUBAN.rs
+++ Rust/NUBAN.rs
@@ -1,3 +1,23 @@
+/// NUBAN verification algorithm
+///
+/// How to Use:
+///
+/// Once you have successfuly installed rust
+/// run the following commands
+///
+/// ```bash
+///     rustc NUBAN.rs
+/// ```
+/// Then run
+///
+/// ```bash
+///     ./NUBAN BANK_CODE ACCONT_NUMBER
+/// ```
+///
+/// Replace `BANK_CODE` and `ACCOUNT_NUMBER` with real numbers
+///
+
+
 use std::process;
 use std::env;
 
@@ -15,6 +35,11 @@ fn main() {
 fn is_valid(args: &[String]) -> Result<bool, bool> {
     let bank_code = args[1].clone();
     let mut account_number = args[2].clone();
+
+    if bank_code.len() != 3 || account_number.len() != 10 {
+        panic!("Invalid arguments, please check your input");
+    }
+
     let check_digit = account_number.pop().unwrap();
 
     let mut number = String::new();
