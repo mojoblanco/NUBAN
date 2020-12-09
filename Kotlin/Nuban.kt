4d48e3c219d295eeaf6c2a26095001c8344e1c6e
laurent russier <laurent.russier@gmail.com>
2020-12-09 17:14:13 +0100

    Update Nuban validation
    Number validator according new rules.

--- Kotlin/Nuban.kt
+++ Kotlin/Nuban.kt
@@ -1,78 +1,44 @@
-import java.util.*
+package com.fairmoney.domain.bank
 
-class Nuban {
+/**
+ * NUBAN standards: https://www.cbn.gov.ng/Out/2020/PSMD/REVISED%20STANDARDS%20ON%20NIGERIA%20UNIFORM%20BANK%20ACCOUNT%20NUMBER%20(NUBAN)%20FOR%20BANKS%20AND%20OTHER%20FINANCIAL%20INSTITUTIONS%20.pdf
+ */
+class NubanValidator {
 
-    val banks = mapOf (
-            "044" to "Access Bank",
-            "014" to "Afribank",
-            "023" to "Citibank",
-            "063" to "Diamond Bank",
-            "050" to "Ecobank",
-            "040" to "Equitorial Trust Bank",
-            "011" to "First Bank",
-            "214" to "FCMB",
-            "070" to "FinBank",
-            "058" to "Guarantee Trust Bank",
-            "069" to "Intercontinental Bank",
-            "056" to "Oceanic Bank",
-            "082" to "BankPhb",
-            "076" to "Skye Bank",
-            "084" to "SpringBank",
-            "221" to "StanbicIBTC",
-            "068" to "Standard Chartered Bank",
-            "033" to "United Bank For Africa",
-            "032" to "Union Bank",
-            "035" to "Wema Bank",
-            "057" to "Zenith Bank",
-            "215" to "Unity Bank" )
+    private companion object {
+        /**
+         *  DMB: Deposit Money Banks
+         */
+        private const val DMB_BANK_CODE_LENGTH = 3
 
-    val algo = intArrayOf(3, 7, 3, 3, 7, 3, 3, 7, 3, 3, 7, 3)
-
-
-    fun validateNubanAccount(bankCode: String, accountNumber: String): Boolean {
-
-        if (bankCode.length != 3 || accountNumber.length != 10) {
-            return false
-        }
-
-        val nubanSerialNo = accountNumber.substring(0, accountNumber.length - 1)
-        val nubanAccNo = bankCode + nubanSerialNo
-
-        val checkDigit = Character.getNumericValue(accountNumber[accountNumber.length - 1])
-
-        var sum = 0
-        for (j in 0..nubanAccNo.length - 1) {
-            sum += Character.getNumericValue(nubanAccNo[j]) * algo[j]
-        }
-
-        val validatedCheckDigit = 10 - (sum % 10)
-
-        return checkDigit == (validatedCheckDigit % 10);
-
-    }
-}
-
-
-fun main(args : Array<String>) {
-
-    println("Validate Nuban Account")
-    println("----------------------")
-
-    val input1 = Scanner(System.`in`)
-    println("Enter Account Number: ")
-    val accountNumber = input1.next()
-
-    val input2 = Scanner(System.`in`)
-    println("Enter Bank Code: ")
-    val bankCode = input2.next()
-
-
-    val response = Nuban().validateNubanAccount(bankCode, accountNumber)
-    if(response == true){
-        print("The account number is Valid")
-    } else {
-        print("The account number is Invalid")
+        /**
+         * OFI: Other Financial Institutions
+         */
+        private const val OFI_BANK_CODE_LENGTH = 5
+        private val ALGORITHM = intArrayOf(3, 7, 3, 3, 7, 3, 3, 7, 3, 3, 7, 3, 3, 7, 3)
     }
 
+    fun validate(bankCode: String, accountNumber: String): Boolean {
+        require(accountNumber.length == 10) { "Account number must be 10 digits long" }
 
+        val formattedBankCode = when (bankCode.length) {
+            3 -> "000$bankCode"
+            5 -> "9$bankCode"
+            else -> throw IllegalArgumentException("Bank code must be 3 or 5 digits")
+        }
+
+        val nubanSerialNumber = accountNumber.take(9)
+        val checkDigit = Character.getNumericValue(accountNumber.last())
+
+        val validatedCheckDigit = 10
+                .minus(
+                        (formattedBankCode + nubanSerialNumber)
+                                .mapIndexed { index, char -> Character.getNumericValue(char) * ALGORITHM[index] }
+                                .sum()
+                                .rem(10)
+                )
+                .rem(10)
+        
+        return checkDigit == validatedCheckDigit
+    }
 }
